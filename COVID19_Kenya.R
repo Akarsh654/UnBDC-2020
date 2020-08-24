3861763f64484047d248ae54a6a41432d9d57b2b
Akrash Sharma <akrash@ualberta.ca>
2020-08-24 00:33:58 -0600

    Add files via upload

---
+++ COVID19_Kenya.R
@@ -0,0 +1,46 @@
+setwd('C:/Users/akars/Desktop/Studies/EXTRA/UBDC')
+library(ggplot2)
+library(plotly)
+library(data.table)
+library(caTools)
+
+# Read the data 
+kenya <- fread("Kenya.csv",drop=c(1,4))
+
+# Scatter Plot with regression line
+pl <- ggplot(kenya,aes(x=total_flights,y=total_cases))
+pl <- pl + geom_point() + geom_smooth(method = "lm", se = FALSE) + theme_bw() 
+pl <- ggplotly(pl)
+print(pl)
+
+# Set a Seed
+set.seed(101)
+
+# Split up sample 
+sample <- sample.split(kenya$total_flights,SplitRatio = 0.7)
+
+# Training Data
+train <- subset(kenya, sample==TRUE)
+# Testing Data
+test <- subset(kenya,sample==FALSE) 
+
+# Train and build model
+model <- lm(total_cases~.,data=train)
+print(summary(model))
+
+# Residual
+res <- residuals(model)
+res <- as.data.frame(res)
+
+# Predictions
+cases <- predict(model,test)
+
+
+
+
+
+
+
+
+
+
